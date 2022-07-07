8f0015b435079ec85162210325fbc57892666f1f
Alicja <263696@student.pwr.edu.pl>
2022-07-07 09:26:02 +0200

    Sfml in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "AJG" << std::endl;
+
+
+int main(){
+
+
+
+    sf::RenderWindow window(sf::VideoMode(1600, 720), "Memory Game");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        // check all the windows events that were triggered
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed) window.close();
+
+
+        }
+
+
+
+        window.clear(sf::Color::Black);
+
+
+        window.display();
+    }
     return 0;
 }
